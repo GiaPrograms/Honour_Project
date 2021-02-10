409a1acab34e2baea018bc0900628fa76e5c649c
Amy-Zhou <zxyamyamy@gmail.com>
2021-02-10 17:24:18 -0500

    slider wider broken selection

--- jia-v2.0-master/src/components/UI/Slider/SliderControl.js
+++ jia-v2.0-master/src/components/UI/Slider/SliderControl.js
@@ -10,17 +10,18 @@ import {withStyles} from "@material-ui/core/styles";
 import Slider from "@material-ui/core/Slider";
 
 const PrettoSlider = withStyles({
-  root: {
+  root: { //the whole box
     color: '#52af77',
     height: 20,
     maxWidth: 700,
     padding: '2rem 0 5rem 0',
     margin: '1rem'
   },
-  thumb: {
+  thumb: {//the circle of selection
     height: 34,
     width: 34,
     backgroundColor: '#10434F',
+    //background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     marginTop: -8,
     marginLeft: -12,
     '&:focus,&:hover,&$active': {
@@ -31,33 +32,39 @@ const PrettoSlider = withStyles({
   valueLabel: {
     fontSize: '1.25rem',
     left: 'calc(-50% + 1rem)',
-    top: 'calc(100% + 1.5rem)',
+    top: 'calc(100% + 1.5rem)', //the number for selection
 
     '& *': {
       background: 'transparent',
+      //background: 'linear-gradient(to right,green,orange,yellow,red)',
       color: '#000',
     },
   },
   track: {
-    background: 'none',
+    //background: 'none',
+    background: 'linear-gradient(to right,green,orange,yellow,red)',
     height: 20,
     borderRadius: 6,
   },
   rail: {
     height: 20,
     borderRadius: 8,
+    //background: 'linear-gradient(to right,green,orange,yellow,red)',
     opacity: 1,
   },
 })(Slider);
 
 const SliderControl = props => {
-  const [value, setValue] = useState(7) // Starting value on slider
+  const [value, setValue] = useState(5) // Starting value on slider
   const max = 10 // Max value on slider
   const min = 0 // Min value on slider
   const step = 0.5 // Step change on slider
 
   const [sliderColour, setSliderColour] = useState()
   const [leftColour, setLeftColour] = useState()
+  //added here
+  const[mid1Colour, setMid1Colour] = useState() 
+  const[mid2Colour, setMid2Colour] = useState()
   const [rightColour, setRightColour] = useState()
 
   /**
@@ -75,13 +82,23 @@ const SliderControl = props => {
 
   useEffect(() => {
     if(props.reversed === true) {
-      setSliderColour('reversed')
+      /*setSliderColour('reversed')
       setLeftColour('#4DAA33')
-      setRightColour('#B10921')
+      setRightColour('#B10921')*/
+      setSliderColour('reversed')
+      setLeftColour('green')
+      setMid1Colour('orange')
+      setMid2Colour('yellow')
+      setRightColour('red')
     } else {
-      setSliderColour('normal')
+      /*setSliderColour('normal')
       setLeftColour('#B10921')
-      setRightColour('#4DAA33')
+      setRightColour('#4DAA33')*/
+      setSliderColour('normal')
+      setLeftColour('red')
+      setMid2Colour('yellow')
+      setMid1Colour('orange')
+      setRightColour('green')
     }
    
   }, [props.reversed])
