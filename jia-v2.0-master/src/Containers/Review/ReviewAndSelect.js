99ed40b6dbe58dd9e166c917fc1ecf2d2e6f9cda
Amy-Zhou <zxyamyamy@gmail.com>
2021-04-09 18:32:38 -0400

    fix bug with nextbtn doesn't work on step3to4

--- jia-v2.0-master/src/Containers/Review/ReviewAndSelect.js
+++ jia-v2.0-master/src/Containers/Review/ReviewAndSelect.js
@@ -5,16 +5,26 @@ import Recommendations from "../../components/Review/Recommendations";
 import Favourites from "../../components/Review/Favourites";
 import TabNavReview from '../../components/Review/TabNavReview';
 import BottomTabNavReview from '../../components/Review/BottomTabNavReview';
+import DialogBox from "../../components/UI/DialogBox";
+import FailedSaveDialog from "../../components/UI/FailedSaveDialog";
 
 import Footer from "../../components/Footer";
 import Subheader from '../../components/UI/Subheader/Subheader';
 import NavigationButton from '../../components/UI/Buttons/NavigationButton';
 import {getRequest} from "../../API/ApiHandler"
+import NavigationDialog from "../../components/UI/NavigationDialog"
 
 const ReviewAndSelect = props => {
   const [treatments, setTreatments] = useState([])
   const [favourites, setFavourites] = useState()
   const [navTo, setNavTo] = useState('')
+  const [navBy, setNavBy] = useState('')
+  const [saved, setSaved] = useState(true)
+  const [displayNavDialog, setDisplayNavDialog] = useState(false)
+  const [didSelect, setDidSelect] = useState(false)
+  const [open, setOpen] = useState(false)
+  
+  
 
   const getUserFavourite = async() => {
     let data = await getRequest(`/userFavourites/user`)
@@ -29,9 +39,36 @@ const ReviewAndSelect = props => {
     if(data.length > 0) setTreatments(data)
   }
 
-  const handleStepperNav = to => {
-    props.history.push(to)
+  // const handleStepperNav = to => {
+  //   props.history.push(to)
+  // }
+
+  const handleButtonNav = () => {
+    if (saved) {
+      props.history.push(`/make-your-plan`)
+    } else {
+      setNavBy('button')
+      setDisplayNavDialog(true)
+    }
   }
+
+  const handleStepperNav = to => {
+    if (saved) {
+      props.history.push(to)
+    } else {
+      setNavBy('stepper')
+      setDisplayNavDialog(true)
+    }
+  }
+
+  const closeNavDialog = selection => {
+    selection === 0
+      ? setDisplayNavDialog(false)
+      : navBy === "button"
+        ? props.history.push(`/review-and-select`)
+        : props.history.push(navTo)
+  }
+
 
   useEffect(() => {
     window.scrollTo(0, 0)
@@ -82,8 +119,12 @@ const ReviewAndSelect = props => {
         title={<>If you wish to see more options, you can also click on the <strong>“All Treatments”</strong><br/>tab or go back to change your preferences in Step 2.</>}
         btnText="Continue to Step 4"
         path="/make-your-plan"
+        handleNavigation={handleButtonNav}
       />
       <Footer/>
+      {!didSelect && <DialogBox description="The selected information will be saved in the trial database. You can modify the information as needed." step='s3Trial' />}
+      <NavigationDialog open={displayNavDialog} handleClose={closeNavDialog} />
+      <FailedSaveDialog open={open} setOpen={setOpen} />
     </div>
   )
 }
