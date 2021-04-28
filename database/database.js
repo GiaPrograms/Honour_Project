e99829731e1878e9f75c4b1f1ab1f9ddb07b355f
GiaPrograms <legianguyen1999@gmail.com>
2021-04-28 10:01:00 -0400

    changed database connection and update jia.sql file

--- database/database.js
+++ database/database.js
@@ -1,21 +1,8 @@
 const Sequelize = require('sequelize');
 
 // ! update DB
-const db = new Sequelize('JIA', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: true,
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
-
-// const db = new Sequelize('heroku_32d76f0ab399d58', 'b7fdc8cf25aab7', '9ddf878d', {
-//   host: 'us-cdbr-east-03.cleardb.com',
+// const db = new Sequelize('JIA', 'root', '', {
+//   host: 'localhost',
 //   dialect: 'mysql',
 //   logging: true,
 
@@ -26,6 +13,19 @@ const db = new Sequelize('JIA', 'root', '', {
 //     idle: 10000
 //   }
 // });
+
+const db = new Sequelize('jia-optionmap-database', 'admin', 'admin', {
+  host: 'us-mm-dca-ec51fe76a795.g5.cleardb.net',
+  dialect: 'mysql',
+  logging: true,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
 
 db.authenticate()
   .then(() => console.log("Connected to the Database"))
