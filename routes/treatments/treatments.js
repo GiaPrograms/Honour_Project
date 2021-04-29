17217e00458aac604ce0c8e2bf8e2fe3861ba3f7
GiaPrograms <legianguyen1999@gmail.com>
2021-04-28 22:03:29 -0400

    attempting to remove uploads codes

--- routes/treatments/treatments.js
+++ routes/treatments/treatments.js
@@ -33,9 +33,9 @@ const treatmentsController = require('../../controllers/treatments/treatments')
 router
 .get('/', treatmentsController.getAll)
 .get('/:id', treatmentsController.getOne)
-.post('/', isAuth, upload.single('image'), treatmentsController.add)
+// .post('/', isAuth, upload.single('image'), treatmentsController.add)
 .post('/some', isAuth, treatmentsController.getSome)
-.patch('/:id', isAuth, upload.single('image'),treatmentsController.update)
+// .patch('/:id', isAuth, upload.single('image'),treatmentsController.update)
 .patch('/reorder/list', isAuth, treatmentsController.updateOrder)
 .delete('/:id', isAuth, treatmentsController.delete)
 
