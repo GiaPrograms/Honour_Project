44e12218e76fe8a8aad9e2d1fa6191fbe48a3ff3
GiaPrograms <legianguyen1999@gmail.com>
2021-04-28 23:33:31 -0400

    testing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let db = require('./database/database')
 app.use(express.json())
 app.use(cookieParser())
 // ! Update origin
-// app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 app.use('/uploads', express.static('uploads'))
 
 app.use('/auth', require('./routes/auth'))
@@ -58,6 +58,8 @@ app.use('/logs', require('./routes/logs'))
 
 app.use(require('./middleware/logErrors'))
 app.use(require('./middleware/errorHandler'))
+
+app.get('/auth', require('./routes/auth'))
 
 db.sync()
 
