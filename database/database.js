1c289306970bf1083736e8e55ce9fc96a8a0a0f1
unknown <ngraham604@gmail.com>
2021-05-01 15:23:01 -0400

    date range on dashboard

--- database/database.js
+++ database/database.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('JIA', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
-  logging: true,
+  logging: false,
 
   pool: {
     max: 5,
